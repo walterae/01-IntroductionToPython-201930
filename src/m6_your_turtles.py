eb679a61c9a94d9271dcd9248e2cf9f12e1ced47
Audrey Walters <walterae@rose-hulman.edu>
2019-03-04 09:22:49 -0500

    Progress

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Audrey Walters.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,33 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+
+#Define turtle jack who draws with a blue pen at a speed of 5
+
+jack = rg.SimpleTurtle('turtle')
+jack.pen = rg.Pen('blue', 1)
+jack.speed = 5
+
+for j in range (50):
+    jack.forward(j)
+    jack.right(50)
+    jack.backward(j-10)
+
+#Define turtle jill who draws with a red pen at a speed of 8
+
+jill = rg.SimpleTurtle('turtle')
+jill.pen = rg.Pen('red', 1)
+jill.speed = 8
+radius = 100  #radius of circle
+
+for j in range (4):
+
+    jill.draw_circle(radius)
+    jill.backward(5)
+    radius = radius - 10
+
+
+window.close_on_mouse_click()
+
